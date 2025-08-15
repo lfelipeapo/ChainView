5ad2099b282e122dbbe430a5555657d45990aba9
Luiz Felipe Apolinário <lfelipeapo@gmail.com>
2025-08-15 16:53:06 -0300

    fix: corrigir rota raiz para produção

--- applications/doc-viewer/routes/web.php
+++ applications/doc-viewer/routes/web.php
@@ -3,14 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // Detecta automaticamente a URL do frontend baseado no ambiente
-    if (app()->environment('production')) {
-        // Em produção, usa a mesma URL do backend mas porta 3000
-        $backendUrl = request()->getSchemeAndHttpHost();
-        $frontendUrl = str_replace([':80', ':443'], ':3000', $backendUrl);
-    } else {
-        // Em desenvolvimento local
-        $frontendUrl = 'http://localhost:3000';
-    }
+    $frontendUrl = env('FRONTEND_URL', 'http://localhost:3000');
     return redirect($frontendUrl);
 });
