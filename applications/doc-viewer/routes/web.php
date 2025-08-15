b7d1f309d37bbc031ad233e225e4d962ff2fa7d5
Luiz Felipe Apolinário <lfelipeapo@gmail.com>
2025-08-15 17:13:49 -0300

    fix: web returns to localhost:3000 only local first

--- applications/doc-viewer/routes/web.php
+++ applications/doc-viewer/routes/web.php
@@ -6,8 +6,5 @@ Route::get('/', function () {
     // Detecta automaticamente a URL do frontend
     if (request()->getHost() === 'localhost' || request()->getHost() === '127.0.0.1') {
         return redirect('http://localhost:3000');
-    } else {
-        // Em produção, redireciona para a mesma URL
-        return redirect(request()->getSchemeAndHttpHost());
     }
 });
