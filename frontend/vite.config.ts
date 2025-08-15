cca2cae0cc92be070a005d2bcf0e91f3642df699
Luiz Felipe Apolinário <lfelipeapo@gmail.com>
2025-08-15 14:53:21 -0300

    ci: deploy pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ChainView/' : '/',
+  base: '/ChainView/', // importante para assets no GitHub Pages
   server: {
     port: 3000,
     host: true
