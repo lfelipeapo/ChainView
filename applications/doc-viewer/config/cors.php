87cf64b95b7055d5f51de601ab3f49a956263336
Luiz Felipe Apolinário <lfelipeapo@gmail.com>
2025-08-14 22:18:38 -0300

    chore: security vunerabilities

--- applications/doc-viewer/config/cors.php
+++ applications/doc-viewer/config/cors.php
@@ -25,7 +25,9 @@ return [
         'http://localhost:8080',
         'http://127.0.0.1:8080',
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'http://localhost',
+        'http://127.0.0.1'
     ],
 
     'allowed_origins_patterns' => [
